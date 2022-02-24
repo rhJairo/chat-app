7f73760849bd0808abcf7b24acfbf080bc536c8b
Jairo Rubio <rh.jairo.94@gmail.com>
2022-02-24 11:32:02 -0600

    implemented header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function Header(props){
-    
+    function SignOut(){
+        return props.auth.currentUser && (
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        )
+    }
 
     return(
         <header className='App-header'>
-            
+        Chat App
+            <div>
+                <SignOut />
+            </div>
         </header>
     )
 }
