870277bb411ab86ff783960eb90177f008eb76fc
Jairo Rubio <rh.jairo.94@gmail.com>
2022-02-24 11:06:15 -0600

    implemented components created and exported to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from'react';
 import './App.css';
 import ChatRoom from './Components/ChatRoom'
 import SignIn from './Components/SignIn'
+import Header from './Components/Header'
 
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, query, orderBy, limit, serverTimestamp, addDoc } from 'firebase/firestore'
+import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+//hooks
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {useCollectionData} from 'react-firebase-hooks/firestore'
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyB9D0Q2Lmkby3D9CRPUdvpH5a_6iXzwShQ",
   authDomain: "chat-app-d7092.firebaseapp.com",
   projectId: "chat-app-d7092",
@@ -18,18 +20,20 @@ firebase.initializeApp({
   messagingSenderId: "404180275065",
   appId: "1:404180275065:web:23406e9d1077b7594d69a8",
   measurementId: "G-4JYNYLM1TM"
-})
+}
 
-const auth = firebase.auth()
-const firestore = firebase.firestore()
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
 
 function App() {
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        { user ? <ChatRoom /> : <SignIn className='sign-in' /> }
       </section>
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header className='App-header'>
+        Chat App
+            <div>
+                
+            </div>
+        </header>
+    )
+}
+
+export default Header
